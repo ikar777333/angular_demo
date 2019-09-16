0b2070f97ba957787ddb5399bbd8454e06610fa9
ikarak <ikar777333@gmail.com>
2019-09-16 14:39:52 +0300

    WIP

--- src/app/components/diagram/diagram.component.ts
+++ src/app/components/diagram/diagram.component.ts
@@ -11,6 +11,7 @@ import { ChooseBusbarDialogComponent } from "../ChooseBusbarDialog/ChooseBusbarD
 
 import * as d3 from 'd3';
 
+
 @Component({
   selector: 'app-diagram',
   encapsulation: ViewEncapsulation.None,
@@ -19,12 +20,12 @@ import * as d3 from 'd3';
 })
 export class DiagramComponent implements OnInit {
 
-  private margin = {top: 40, right: 90, bottom: 50, left: 0};
+  private margin = {top: 40, right: 0, bottom: 50, left: 0};
   private width: number;
   private height: number;
   private svg: any;
   private g: any;
-  private treemap: any;
+  private tree: any;
   private nodes: any;
   private loads : Array<Load>;
   private treeData: Array<Node> = [];
@@ -37,7 +38,7 @@ export class DiagramComponent implements OnInit {
 
     this.width = 660 - this.margin.left - this.margin.right;
     this.height = 500 - this.margin.top - this.margin.bottom;
-    this.treemap = d3.tree().size([this.width, this.height]);
+    this.tree = d3.tree().size([this.width, this.height]);
   }
 
   ngOnInit() {
@@ -52,32 +53,37 @@ export class DiagramComponent implements OnInit {
   private initData() {
     this.treeData = this.loads.map(function(element){return new Node(element)});
     this.nodes = d3.hierarchy(this.treeData[0])
-    this.nodes = this.treemap(this.nodes);
-    
+    this.nodes = this.tree(this.nodes); 
   }
 
   private initSvg() {
     this.svg = d3.select("svg")
       .attr("width", this.width + this.margin.left + this.margin.right)
       .attr("height", this.height + this.margin.top + this.margin.bottom)
-    
     this.g = this.svg.append("g")
       .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
   }
 
   private initLinks() {
-
-  // Add the links between nodes:
     this.g.selectAll(".link")
-    .data(this.nodes.descendants().slice(1))
-    .enter().append("path")
-    .attr("class", "link")
-    .attr("d", function(d) {
-       return "M" + d.x + "," + d.y
-         + "C" + d.x + "," + (d.y + d.parent.y) / 2
-         + " " + d.parent.x + "," +  (d.y + d.parent.y) / 2
-         + " " + d.parent.x + "," + d.parent.y;
-       });
+      .data(this.nodes.links(this.nodes))
+      .enter()
+      .append('path')
+      .attr("class", "link")
+      .attr("d", function(d) {
+        return "M" + (d.source.x + 25) + "," + d.source.y
+          + "C" + (d.source.x + 25) + "," + (d.source.y + d.target.y) / 2
+          + " " + (d.target.x + 25) + "," +  (d.source.y + d.target.y) / 2
+          + " "+ (d.target.x + 25) + "," + d.target.y;
+      });
+  }
+
+  private nodeId(d) {
+    return d.data.load.$id;
+  }
+
+  private linkId(d) {
+    return d.source.data.load.$id + "-" + d.target.data.load.$id;
   }
 
   private initNodes(){
@@ -89,16 +95,26 @@ export class DiagramComponent implements OnInit {
         (d.children ? " node--internal" : " node--leaf"); })
     .attr("transform", function(d) { 
       return "translate(" + d.x + "," + d.y + ")"; });
-    node.append("circle")
-      .attr("r", 10)
+
+    node.append("rect")
+      .attr("width", 50)
+      .attr("height", 25)
       .on('click', (d) => {
         this.openLoadDialog(d.data.load);
       })
+
     node.append("text")
+      .attr("dy", ".35em")
+      .attr("x", function(d) { return 25 })
+      .attr("y", function(d) { return d.children ? -20 : 40; })
+      .style("text-anchor", "middle")
+      .text(function(d) { return d.data.load.$name; });
+
+    /*node.append("text")
       .attr("dy", ".35em")
       .attr("y", function(d) { return d.children ? -20 : 20; })
       .style("text-anchor", "middle")
-      .text(function(d) { return d.data.load.$name; });
+      .text(function(d) { return d.data.load.$name; });*/
   }
 
   private removeLink() {
@@ -118,39 +134,45 @@ export class DiagramComponent implements OnInit {
   }
 
   private openLoadDialog(load: Load): void {
+    console.log("gfg")
     const dialogRef = this.dialog.open(LoadDialogComponent, {
       width: '450px',
       data: load
     });
-
     dialogRef.afterClosed().subscribe(result => {
       this.updateDiagram();
     });
   }
 
   private initZoom() {
-    let svg = this.svg;
-    this.svg.call(d3.zoom().scaleExtent([0.1, 3]).on("zoom", function () {
-      svg.attr("transform", d3.event.transform)
-    }))
+    let g = this.g;
+    this.svg.call(
+      d3.zoom()
+      .on("zoom", function () {
+        g.attr("transform", d3.event.transform)
+    }));
   }
 
   private initDrugAndDrop() {
     this.svg.selectAll(".node").call(d3.drag()
     .on("start", function(d: any) {
-      //d3.select(this).raise().attr("stroke", "black")
     })
     .on("drag", function(d: any) {
-      d3.select(this).attr("transform", "translate(" + (d.x = d3.event.x) + "," + (d.y = d3.event.y) + ")")
+      d3.select(this).attr("transform", "translate(" + (d.x = d3.event.x) + "," + (d.y = d3.event.y) + ")");
     })
     .on("end", (d: any) => {
-      this.updateLink()
+      this.updateLink(d)
     }));
   }
 
-  private updateLink() {
+  private onOverlay() {
+    
+  }
+
+  private updateLink(d: any) {
     this.svg.selectAll('.link').remove();
     this.initLinks();
+    this.g.selectAll(".node").raise();
   }
 
 }
